62330dae4ea7bf92e7a1b6a780e7985cf86ef87a
megdlawit <megdalenea@gmail.com>
2023-01-03 11:16:24 +0300

    get_ids_sum

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
